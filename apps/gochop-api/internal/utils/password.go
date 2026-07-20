1e669d09267ca2f90bacb74f2dd6ef4b6608e2d3
Nelson Obire <vegoinc10@gmail.com>
2026-07-20 20:21:14 +0100

    Add password hashing utility

--- apps/gochop-api/internal/utils/password.go
+++ apps/gochop-api/internal/utils/password.go
@@ -1,1 +1,14 @@
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword hashes a plain text password.
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CheckPassword compares a hashed password with a plain text password.
+func CheckPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
